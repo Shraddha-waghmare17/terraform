e6cf76c5de1aec6fbaf54e0d2b62a808b7bb81e4
Shraddha waghmare <waghmareshraddha365@gmail.com>
2026-09-07 17:29:51 +0530

    day2-module

---
+++ module/asg/main.tf
@@ -0,0 +1,25 @@
+resource "aws_instance" "public_instance" {
+    ami = var.ami
+    instance_type = var.instance_type
+    key_name = var.key_name
+    subnet_id = var.public_subnet_id
+    vpc_security_group_ids = [var.sg_id]
+    associate_public_ip_address = true
+    user_data = ("/root/terraform/day-2-tf-varfile/user_data.sh")
+    tags = {
+        Name = "public_instance"
+    }
+}
+
+resource "aws_instance" "private_instance" {
+    ami = var.ami
+    instance_type = var.instance_type
+    key_name = var.key_name
+    subnet_id = var.private_subnet_id
+    vpc_security_group_ids = [var.sg_id]
+    associate_public_ip_address = false
+    user_data = file("/root/terraform/day-2-tf-varfile/user_data.sh")
+    tags = {
+        Name = "private_instance"
+    }
+}
